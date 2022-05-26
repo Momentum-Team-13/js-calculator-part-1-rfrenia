12151210b23c46233c524908051b3609bdeff633
rfrenia <ryanfrenia@gmail.com>
2022-05-25 22:39:33 -0400

    calculator

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log("hi world")
+
+
+
+
+{ const clearButton = document.querySelector('button.clear')
+function buttonclick (){
+    alert('clear')
+}
+clearButton.addEventListener('click', buttonclick)
+}
+
+{ const numberButton = document.querySelector('button.number')
+function buttonclick (){
+    alert(7)
+    }
+
+numberButton.addEventListener('click', buttonclick)
+}
+
+//getElementById
