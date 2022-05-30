5594483310955020c2d8cc9500f513db1c6310d8
rfrenia <ryanfrenia@gmail.com>
2022-05-30 17:25:07 -0400

    calculatorNotes

--- main.js
+++ main.js
@@ -9,6 +9,12 @@ let displayText = '';
 let operator;
 let stored;
 
+//simplifying the display content
+function updateDisplayText() {
+  display.textContent = displayText
+}
+
+//evaluating mathematical expressions
 function total() {
   const displayNumber = Number(displayText);
   if (operator === "+" ) {
@@ -21,21 +27,24 @@ function total() {
     displayText = stored / displayNumber; 
   } 
 
-  display.textContent = displayText  
+  updateDisplayText()  
 }
 
+//clearing the calculator input
 function clearingDisplay(){
   displayText = '';
   operator = '';
-  display.textContent = displayText;
+  updateDisplayText();
 }
 
+//displays the number clicked
 function updateNumberDisplay(event) {
   console.log('clicked on number/decimal button', event.target.textContent);
   displayText += event.target.textContent;
-  display.textContent=displayText;
+  updateDisplayText();
 }
 
+//truthy statement to run the "total" function each time an operator is clicked
 function addOperator(event) {
   console.log('clicked on symbol', event.target.textContent);
 
@@ -60,13 +69,3 @@ for (let symbol of symbols) {
 
 equalButton.addEventListener ('click', total)
 clearButton.addEventListener('click', clearingDisplay)
-
-// // //getElementById
-
-// // //eventDelegation
-
-// // //(event.target.textContent) ***
-
-// //onclick="print()" 
-
-// //display.appendChild(nameoftheelement
