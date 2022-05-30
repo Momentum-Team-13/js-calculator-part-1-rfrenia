ab050bcc86d6b1b25e9f1c04c2c6e17a98270595
rfrenia <ryanfrenia@gmail.com>
2022-05-30 11:28:42 -0400

    calculator

--- main.js
+++ main.js
@@ -1,142 +1,61 @@
 
-let keys = document.querySelectorAll('.number');
-console.log(keys);
+const keys = document.querySelectorAll('.number');
+const symbols = document.querySelectorAll('.symbol');
+const display = document.querySelector('.display');
+const equalButton = document.querySelector('#equal');
+const clearButton = document.querySelector('#clear');
 
-let symbols = document.querySelectorAll('.symbol');
-
-let display = document.querySelector('.display');
-
-let result = ''
-
-let equalButton = document.querySelector('#equal');
-
-let clearButton = document.querySelector('#clear')
-
-
-console.log(equalButton);
-console.log(clearButton);
-console.log(keys[2].classList);
-console.log(display)
-
-
-let displayValue ;
-let operator ;
-
-for (let number of keys) {
-    number.addEventListener('click', function (event) {
-    console.log(event.target.textContent);
-    result += (event.target.textContent);
-    display.textContent=result;
-    displayValue = Number (result) ;
-     })}
-
-    let stored ;
-
-for (let symbol of symbols) {
-    symbol.addEventListener('click', function (event) {
-    console.log(event.target.textContent);
-    operator = symbol.textContent ;
-    stored = displayValue ;
-    result = '' ;
-    })}
-
+let result = '';
+let displayValue;
+let operator;
+let stored;
 
 function total() {
-    if (operator === "+" ) {
-    displayValue = displayValue + stored }
-    else if (operator === "X" ) {
-    displayValue = displayValue * stored }
-    else if (operator === "-" ) {
-    displayValue = stored - displayValue }
-    else if (operator === "/" ) {
-    displayValue = stored / displayValue }
-    display.textContent = displayValue 
-}
-equalButton.addEventListener ('click', total)
+  if (operator === "+" ) {
+    displayValue = displayValue + stored
+  } else if (operator === "X" ) {
+    displayValue = displayValue * stored 
+  } else if (operator === "-" ) {
+    displayValue = stored - displayValue 
+  } else if (operator === "/" ) {
+    displayValue = stored / displayValue 
+  }
 
+  display.textContent = displayValue
+}
 
 function clearingDisplay(){
-    displayValue = ''
-    result = ''
-    display.textContent = displayValue
+  displayValue = ''
+  result = ''
+  display.textContent = displayValue
 }
 
+function updateNumberDisplay(event) {
+  console.log('clicked on number/decimal button', event.target.textContent);
+  result += event.target.textContent;
+  display.textContent=result;
+  displayValue = Number(result);
+}
+
+function addOperator(event) {
+  console.log('clicked on symbol', event.target.textContent);
+  operator = symbol.textContent ;
+  stored = displayValue ;
+  result = '' ;
+}
+
+// (let number of keys) when numbers and decimal are clicked they
+//display and convert from string into numbers for calculating.
+for (let number of keys) {
+  number.addEventListener('click', updateNumberDisplay)
+}
+
+for (let symbol of symbols) {
+  symbol.addEventListener('click', addOperator)
+}
+
+equalButton.addEventListener ('click', total)
 clearButton.addEventListener('click', clearingDisplay)
-  
-
-
-
-    // number.addEventListener('click', function (event) {
-    //     function buttonclick (){
-    //         result += (event.target.textContent);
-    //         display.textContent=result
-    // } )
-
-    // for (let number of keys) {
-    //     number.addEventListener('click', function (event) {
-    //         document.getElementById("a").innerHTML = event.target.textContent
-    
-    //     })}
-     
-
-//   function print() {
-//     document.getElementById("a").innerHTML = 
-//          numberButton = document.querySelector('button.number')
-//          function buttonclick() {
-//             alert(7)
-//             } 
-//    numberButton.addEventListener('click', buttonclick)
-// }
-//     ;
-
-
-// let eight = document.getElementById("8").textContent
-// let seven = document.getElementById("7").textContent
-
-
-
-// { const clearButton = document.querySelector('button.clear')
-// function buttonclick (){
-//     document.getElementById("a").innerHTML = ''
-// }
-// clearButton.addEventListener('click', buttonclick)
-// }
-
-// //    function print() {
-// //      document.getElementById("a").innerHTML = 
-
-// { const numberButton = document.querySelector('button.number7')
-// function buttonclick (){
-//     document.getElementById("a").innerHTML = seven
-// }
-// numberButton.addEventListener('click', buttonclick)
-// }
-
-// {  
-// const numberButton = document.querySelector('button.number8')
-// function buttonclick (){
-//     document.getElementById("a").innerHTML = eight
-//     }
-// numberButton.addEventListener('click', buttonclick)
-// }
-
-// // { const numberButton = document.querySelector('button.number9')
-// //     numberButton.addEventListener('click', function (event) {
-// //    event.target.textContent
-// //   })
-// // }
-//   // }
-
-
-
-// // // for let button.number of numberButton {
-// // //     button.number.addEventListener('click', function (event) {
-// // //     console.log(event.target)
-// // //     // event.target.classList.remove("unhighlight");
-// // //     // event.target.classList.add("highlight");
-// // //     }
-// // // }
-
 
 // // //getElementById
 
